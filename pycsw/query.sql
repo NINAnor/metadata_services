7ed8b630a1e613c01ec73af53804cd9932112baa
Niccolo Cantu <niccolo.cantu@nina.no>
2025-09-01 16:23:26 +0200

    chg: remove hardcoded references

--- pycsw/query.sql
+++ pycsw/query.sql
@@ -1,11 +1,17 @@
 load sqlite;
-load httpfs;
 
+load httpfs;
 
 ATTACH IF NOT EXISTS 'data/records.db' AS sqlite_db (TYPE sqlite);
 
-delete from sqlite_db.records where 1 = 1;
+delete from sqlite_db.records
+where
+    1 = 1;
 
-insert into sqlite_db.records by name (
-    from '${RECORDS_PARQUET_PATH}' select * exclude (metadata)
-);
+insert into
+    sqlite_db.records by name (
+        from
+            '${CSW_RECORDS_PARQUET_PATH}'
+        select
+            * exclude (metadata)
+    );
--- pygeoapi/setup-conf.py
+++ pygeoapi/setup-conf.py
@@ -3,14 +3,21 @@ import json
 import yaml
 import pathlib
 import s3fs
+import environ
+
+env = environ.Env()
+
+S3_ENDPOINT_URL = env("ENDPOINT_URL")
+S3_BUCKET = env("S3_BUCKET")
+S3_PATH = env("S3_PATH", default="geoapi/*.json")
 
 base_config = yaml.load(pathlib.Path("base.yml").open(), yaml.SafeLoader)
 
-s3 = s3fs.S3FileSystem(anon=True, endpoint_url="https://s3-int-1.nina.no")
+s3 = s3fs.S3FileSystem(anon=True, endpoint_url=S3_ENDPOINT_URL)
 
 resources = {}
 
-resource_files = s3.glob("miljodata-test/geoapi/*.json")
+resource_files = s3.glob(f"{S3_BUCKET}{S3_PATH}")
 
 for resource_file in resource_files:
     for resource in json.loads(s3.open(resource_file).read()):
