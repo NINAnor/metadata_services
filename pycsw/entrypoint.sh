98bf068ad348a3adf26b008aed91b91f5a992efc
Niccolo Cantu <niccolo.cantu@nina.no>
2025-09-11 13:20:53 +0200

    chg: allow to fail

--- pycsw/entrypoint.sh
+++ pycsw/entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 echo 'pulling data'
 cp base.db data/records.db
-duckdb ":memory:" "$(envsubst <query.sql)"
+duckdb ":memory:" "$(envsubst <query.sql)" 2>&1 || true
 python3 /usr/local/bin/entrypoint.py
