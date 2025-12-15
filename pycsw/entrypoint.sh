38790f1b8aef46a8e9a8ef71a5766b47cd323119
Niccolo Cantu <niccolo.cantu@nina.no>
2025-12-15 15:10:08 +0100

    chg: add more logging, force copy

--- pycsw/entrypoint.sh
+++ pycsw/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 set -e
+echo 'reset database'
+yes | cp -rf base.db data/records.db
 echo 'pulling data'
-cp base.db data/records.db
 duckdb ":memory:" "$(envsubst <query.sql)" 2>&1 || true
+echo 'loaded data'
 python3 /usr/local/bin/entrypoint.py
